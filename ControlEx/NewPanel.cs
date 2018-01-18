2419fb040bb0b8877ca4b3ad50fa6a2ea4b140e5
coderlfy <13403692778@126.com>
2018-01-18 09:47:55 +0800

    3

---
+++ ControlEx/NewPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ControlEx
+{
+    public partial class NewPanel : Panel
+    {
+        public NewPanel()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;//设置本窗体
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true); // 禁止擦除背景.
+            SetStyle(ControlStyles.DoubleBuffer, true); // 双缓冲
+        }
+        public NewPanel(IContainer container)
+        {
+            container.Add(this);
+            InitializeComponent();
+            this.DoubleBuffered = true;//设置本窗体
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true); // 禁止擦除背景.
+            SetStyle(ControlStyles.DoubleBuffer, true); // 双缓冲
+        }
+    }
+}
